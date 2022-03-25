4b204992303840fb0cb05acd334793e56601a0e0
Gonzalo555 <102135258+gonzaloferraro555@users.noreply.github.com>
2022-03-25 04:27:25 -0300

    Update adivino.py

--- adivino.py
+++ adivino.py
@@ -9,10 +9,10 @@ intento = 1
 while intento < 6 and not gane:
     numero_ingresado = int(input('Ingresa tu número: '))
     if numero_ingresado == numero_aleatorio:
-    print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
-    gane = True
-else:
-    print('Mmmm ... No.. ese número no es... Seguí intentando.')
-    intento += 1
+        print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+        gane = True
+    else:
+        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+        intento += 1
 if not gane:
     print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))
