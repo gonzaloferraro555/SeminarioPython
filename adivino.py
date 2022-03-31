43e47a2a99c1cf05b3f5ed86274d7c2f1bbe857c
Gonzalo Ferraro <gferraro@minfra.gba.gov.ar>
2022-03-31 20:09:31 -0300

    agrego nuevamente

--- adivino.py
+++ adivino.py
@@ -1,13 +1,13 @@
-
-## Adivina adivinador....
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
 import random
-numero_aleatorio = random.randrange(0,100)
+numero_aleatorio = random.randrange(5)
 gane = False
-print("Tenés 5 intentos para adivinar un entre 0 y 100")
+print("Tenés 5 intentos para adivinar un entre 0 y 99")
 intento = 1
 while intento < 6 and not gane:
-    numero_ingresado = int(input('Ingresa tu número: '))
-    if numero_ingresado == numero_aleatorio:
+    numero_Ingresado = int(input('Ingresa tu número: '))
+    if numero_Ingresado == numero_aleatorio:
         print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
         gane = True
     else:
